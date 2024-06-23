d1f9afce1708161ff2e67220056d83d3887dac70
Brian <bw49078@gmail.com>
2024-06-22 22:59:50 -0500

    added example and question to file

---
+++ validPalindrome.js
@@ -0,0 +1,11 @@
+// Given a string s, return true if it is a palindrome, otherwise return false.
+
+// A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+// Example 1:
+
+// Input: s = "Was it a car or a cat I saw?"
+
+// Output: true
+
+const validPalindrome = (s) => {};
