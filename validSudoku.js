29a46ba69aa1d22eba0a89951eac7022bc0d4026
Brian <bw49078@gmail.com>
2024-06-29 21:41:09 -0500

    added example to valid sudoku

---
+++ validSudoku.js
@@ -0,0 +1,27 @@
+// You are given a a 9 x 9 Sudoku board board. A Sudoku board is valid if the following rules are followed:
+
+// Each row must contain the digits 1-9 without duplicates.
+// Each column must contain the digits 1-9 without duplicates.
+// Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without duplicates.
+// Return true if the Sudoku board is valid, otherwise return false
+
+// Note: A board does not need to be full or be solvable to be valid.
+
+// Example 1:
+
+
+
+// Input: board = 
+// [["1","2",".",".","3",".",".",".","."],
+//  ["4",".",".","5",".",".",".",".","."],
+//  [".","9","8",".",".",".",".",".","3"],
+//  ["5",".",".",".","6",".",".",".","4"],
+//  [".",".",".","8",".","3",".",".","5"],
+//  ["7",".",".",".","2",".",".",".","6"],
+//  [".",".",".",".",".",".","2",".","."],
+//  [".",".",".","4","1","9",".",".","8"],
+//  [".",".",".",".","8",".",".","7","9"]]
+
+// Output: true
+
+const validSudoku = (board) => {};
