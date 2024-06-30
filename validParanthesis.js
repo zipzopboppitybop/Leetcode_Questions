96b910c9ac687f297d6266c253532b63373e067a
Brian <bw49078@gmail.com>
2024-06-29 21:37:35 -0500

    added example to file

---
+++ validParanthesis.js
@@ -0,0 +1,18 @@
+// You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+
+// The input string s is valid if and only if:
+
+// Every open bracket is closed by the same type of close bracket.
+// Open brackets are closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+// Return true if s is a valid string, and false otherwise.
+
+// Example 1:
+
+// Input: s = "[]"
+
+// Output: true
+
+const validParanthesis = (s) => {
+
+}
