1aa0c2d82cca9f7cfd6c7eb05afb29fb44f602c1
Brian <bw49078@gmail.com>
2024-06-29 21:39:02 -0500

    finished validParenthis

--- validParanthesis.js
+++ validParanthesis.js
@@ -14,5 +14,29 @@
 // Output: true
 
 const validParanthesis = (s) => {
-
+    let stack = [];
+    let map = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    };
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            stack.push(s[i]);
+        } else {
+            let last = stack.pop();
+            if (s[i] !== map[last]) {
+                return false;
+            }
+        }
+    }
+    if (stack.length !== 0) {
+        return false;
+    }
+    return true;
 }
+
+console.log(validParanthesis("()")); //true
+console.log(validParanthesis("()[]{}")); //true
+console.log(validParanthesis("(]")); //false
+console.log(validParanthesis("([{}])")); //true
