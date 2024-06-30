acdeb6a016fd9bf91c610775d702a76f680150f1
Brian <bw49078@gmail.com>
2024-06-29 21:36:39 -0500

    ceated valid paranthesis file

--- reverseLinkedList.js
+++
@@ -1,9 +0,0 @@
-// Given the beginning of a singly linked list head, reverse the list, and return the new beginning of the list.
-
-// Example 1:
-
-// Input: head = [0,1,2,3]
-
-// Output: [3,2,1,0]
-
-const reverseLinkedList = (head) => {};
