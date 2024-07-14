581fdd77eafc5038204cd795f669176466bb7e99
Brian <bw49078@gmail.com>
2024-07-13 20:40:23 -0500

    completed longest common prefix

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -9,4 +9,16 @@
 // Input: strs = ["flower","flow","flight"]
 // Output: "fl"
 
-const longestCommonPrefix = (strs) => {};
+const longestCommonPrefix = (strs) => {
+    if (strs.length === 0) return '';
+    let prefix = strs[0];
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+        }
+    }
+    return prefix;
+};
+
+console.log(longestCommonPrefix(["flower","flow","flight"])); //fl
+console.log(longestCommonPrefix(["dog","racecar","car"])); //""
