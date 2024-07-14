bff2867b51ffb61c602b5bf49fa571ed6def2eb3
Brian <bw49078@gmail.com>
2024-07-13 20:39:09 -0500

    added explanation and example

---
+++ longestCommonPrefix.js
@@ -0,0 +1,12 @@
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+ 
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+
+const longestCommonPrefix = (strs) => {};
