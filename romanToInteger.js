7f2584783cdb637c0cfa67b3079e55fea8351fd7
Brian <bw49078@gmail.com>
2024-07-13 20:34:30 -0500

    completed roman to integer

--- romanToInteger.js
+++ romanToInteger.js
@@ -25,4 +25,28 @@
 // Output: 3
 // Explanation: III = 3.
 
-const romanToInt = (s) => {};
+const romanToInt = (s) => {
+    let roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (roman[s[i]] < roman[s[i + 1]]) {
+            result += roman[s[i + 1]] - roman[s[i]];
+            i++;
+        } else {
+            result += roman[s[i]];
+        }
+    }
+    return result;
+};
+
+console.log(romanToInt("III")); //3
+console.log(romanToInt("IV")); //4
+console.log(romanToInt("M")); //9
