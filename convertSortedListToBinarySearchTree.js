7bcb28a2dc3edc8b3d59571c7f5a5c588e54182b
Brian <bw49078@gmail.com>
2024-07-20 21:33:45 -0500

    completed convert sorted list

--- convertSortedListToBinarySearchTree.js
+++ convertSortedListToBinarySearchTree.js
@@ -15,4 +15,22 @@
 // Input: head = []
 // Output: []
  
-const sortedListToBST = (head) => {};
+const sortedListToBST = (head) => {
+    let curr = head, count = 0
+    while (curr) curr = curr.next, count++
+    const treeify = (i, j) => {
+        if (j < i) return null 
+        let mid = i + j >> 1, node = new TreeNode()
+        node.left = treeify(i, mid - 1)
+        node.val = curr.val, curr = curr.next
+        node.right = treeify(mid + 1, j)
+        return node
+    }
+    curr = head
+    return treeify(1, count)
+};
+
+
+console.log(sortedListToBST([-10,-3,0,5,9])); //[0,-3,9,-10,null,5]
+console.log(sortedListToBST([])); //[]
+console.log(sortedListToBST([0])); //[0]
